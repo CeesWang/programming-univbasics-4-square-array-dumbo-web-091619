19b0d4ada8381fad6754ba457c1319b474012b12
Chen Wang <wangcees@gmail.com>
2019-08-14 20:44:49 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,9 @@
 def square_array(array)
   counter = 0
+  arr = []
   while counter < array.length
-    array[counter] = array[counter] * array[counter]
+    arr << (array[counter] * array[counter])
     counter += 1
   end
+  return arr
 end
